3e8b937599260608575fd948c025682e2a182cbb
rootie <mck.korzeniewski@gmail.com>
2018-07-24 23:29:32 +0200

    Disable isMounted deprecation warnings

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 /** @format */
 
-import { AppRegistry } from 'react-native';
+import { AppRegistry, YellowBox } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
+
+import * as config from './src/Configuration';
+
+if (!config.IsDebug) {
+    console.disableYellowBox = true;
+} else {
+    YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
+    YellowBox.ignoreWarnings(['Class RCTCxxModule']);
+}
 
 AppRegistry.registerComponent(appName, () => App);
