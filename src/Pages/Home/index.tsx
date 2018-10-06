0a662e5d8fa94117e9357be1f493e0d2841a6457
rootie <mck.korzeniewski@gmail.com>
2018-10-06 08:49:48 -0400

    Setup viroar package

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,12 +1,32 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React, {Component} from 'react'
+import { StyleSheet } from 'react-native'
+import {Text} from 'react-native'
 import AppContainer from '../../Components/AppContainer';
+
+import {
+    ViroARScene,
+    ViroText,
+    ViroConstants,
+} from 'react-viro';
 
 export interface HomeProps {
 }
 
 export function Home(props: HomeProps) {
     return <AppContainer>
-        <Text>Home</Text>
-    </AppContainer >
+            <ViroARScene onTrackingUpdated={console.log("halo")}>
+        <ViroText text={"Siemanko"} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
+      </ViroARScene>
+    </AppContainer>
 }
+
+
+var styles = StyleSheet.create({
+    helloWorldTextStyle: {
+      fontFamily: 'Arial',
+      fontSize: 30,
+      color: '#ffffff',
+      textAlignVertical: 'center',
+      textAlign: 'center',
+    },
+  });
