7d36fd5075164f53e549b0abfc5da7c712d4fd8b
rootie <mck.korzeniewski@gmail.com>
2018-11-16 15:32:38 -0500

    Update react-native 0.57.4 -> 0.57.5

--- tests/Compontents/App.tsx
+++ tests/Compontents/App.tsx
@@ -1,13 +1,1 @@
-// components/__tests__/Hello.tsx
-import React from 'react';
-import renderer from 'react-test-renderer';
-import App from '../../src/App';
 
-
-
-it('renders correctly with defaults', () => {
-    // const text = renderer
-    //     .create(<App />)
-    //     .toJSON();
-    // expect(text).toMatchSnapshot();
-});
